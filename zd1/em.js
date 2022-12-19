897ee8d3ee0b8511127bb0ae2b7eaa1eac7f6d4b
Ivan19629 <shevzov19@gmail.com>
2022-12-20 05:14:32 +0700

    first

--- zd1/em.js
+++ zd1/em.js
@@ -1,9 +1,10 @@
 import getValidEmails from './moduls/getValidEmails.js';
 
 let allList = ['omega1@mail.ru', 'omega2@mail.ru', 'omega3@mail.ru', 'omega4@mail.ru', 'omega5@mail.ru', 'omega6@mail.ru', 'omega7@mail.ru', 'omega8@mail.ru', 'omega9@mail.ru',
- 'nemail1@mail.ru', 'nemail2@mail.ru', 'nemail3@mail.ru', 'nemail4@mail.ru', 'nemail5@mail.ru', 'nemail6@mail.ru', 'nemail7@mail.ru'];
+ 'omega10@mail.ru', 'omega11@mail.ru', 'omega12@mail.ru', 'omega13@mail.ru', 'omega14@mail.ru', 'omega15@mail.ru', 'omega16@mail.ru', 'omega17@mail.ru'];
 
-let blackList = ['omega11@mail.ru', 'omega12@mail.ru', 'omega13@mail.ru', 'omega14@mail.ru', 'omega15@mail.ru', 'omegal6@mail.ru', 'omegal7@mail.ru'];
+
+let blackList = ['omega11@mail.ru', 'omega12@mail.ru', 'omega13@mail.ru', 'omega14@mail.ru', 'omega15@mail.ru', 'omega16@mail.ru', 'omega17@mail.ru'];
  
 console.log(getValidEmails(allList, blackList));
   
