539d21174ea8c23fd2aa43f2e198570b2e6415fa
Ivan19629 <shevzov19@gmail.com>
2022-12-20 05:19:21 +0700

    first

--- zd1/em.js
+++ zd1/em.js
@@ -4,7 +4,7 @@ let allList = ['omega1@mail.ru', 'omega2@mail.ru', 'omega3@mail.ru', 'omega4@mai
  'omega10@mail.ru', 'omega11@mail.ru', 'omega12@mail.ru', 'omega13@mail.ru', 'omega14@mail.ru', 'omega15@mail.ru', 'omega16@mail.ru', 'omega17@mail.ru'];
 
 
-let blackList = ['omega11@mail.ru', 'omega12@mail.ru', 'omega13@mail.ru', 'omega14@mail.ru', 'omega15@mail.ru', 'omega16@mail.ru', 'omega17@mail.ru'];
+let blackList = ['omega18@mail.ru', 'omega19@mail.ru', 'omega20@mail.ru', 'omega21@mail.ru', 'omega22@mail.ru', 'omega23@mail.ru', 'omega24@mail.ru'];
  
 console.log(getValidEmails(allList, blackList));
   
